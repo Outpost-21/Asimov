23e7ab6929f6d479805d6645996f87400dfe264b
neronix17 <39186138+neronix17@users.noreply.github.com>
2024-03-15 11:58:24 +0000

    1.0.8839

---
+++ 1.5/Source/Asimov/AsimovDefOf.cs
@@ -0,0 +1,55 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace Asimov
+{
+    [DefOf]
+    public static class AsimovDefOf
+    {
+        static AsimovDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(AsimovDefOf));
+        }
+
+        public static ResearchProjectDef Asimov_WirelessCharging;
+
+        public static PawnTableDef Asimov_Automatons;
+
+        public static NeedDef Asimov_EnergyNeed;
+
+        public static JobDef Asimov_Hibernate, Asimov_HibernateTillRepaired;
+        public static JobDef Asimov_RepairAutomaton, Asimov_ConsumeEnergySource, Asimov_RechargeFromSocket, Asimov_InsertChargepacks, Asimov_RemoveChargepacks;
+        //public static JobDef Asimov_ChargeOther;
+
+        public static HediffDef Asimov_EmergencyPower;
+
+        public static StatDef Asimov_EnergyMultiplier;
+
+        public static FleshTypeDef Asimov_Automaton;
+
+        public static ThingDef Asimov_HibernationSpot, Asimov_ChargePad, Asimov_WirelessCharger, Asimov_LongRangeWirelessCharger;
+
+
+        public static ThingDef Asimov_Chargepack, Asimov_Chargepack_Empty;
+
+        public static RecipeDef Asimov_CraftChargepacks, Asimov_RechargeChargepack, Asimov_RechargeChargepackBulk;
+
+        // Vanilla
+
+        public static WorkTypeDef BasicWorker, Cooking, Cleaning, Warden, Art, Tailoring;
+
+        public static WorkGiverDef DoctorTendToHumanlikes, DoctorTendToAnimals;
+
+        public static NeedDef DrugDesire;
+
+        public static ThingDef FabricationBench, TableMachining;
+
+        public static ResearchProjectDef Electricity;
+    }
+}
---
+++ 1.5/Source/Asimov/Comps/Comp_Automaton.cs
@@ -0,0 +1,111 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace Asimov
+{
+    public class Comp_Automaton : ThingComp
+    {
+        public CompProperties_Automaton Props => (CompProperties_Automaton)props;
+        public Pawn pawn => parent as Pawn;
+
+        public Color? skinFirst;
+        public Color? skinSecond;
+
+        public bool resolved;
+
+        public List<WorkTypeDef> extraEnabledWorkTypes = new List<WorkTypeDef>();
+
+        public List<WorkTypeDef> cachedEnabledWorkTypes;
+
+        public List<WorkTypeDef> EnabledWorkTypes
+        {
+            get
+            {
+                if (cachedEnabledWorkTypes == null)
+                {
+                    cachedEnabledWorkTypes = new List<WorkTypeDef>();
+                    foreach (WorkTypeDef workType in DefDatabase<WorkTypeDef>.AllDefs)
+                    {
+                        if ((Props.enableAllWorkTypes || extraEnabledWorkTypes.Contains(workType) || (!Props.enabledWorkTypes.NullOrEmpty() && Props.enabledWorkTypes.Contains(workType))) && !cachedEnabledWorkTypes.Contains(workType))
+                        {
+                            if (pawn.RaceProps.Humanlike)
+                            {
+                                pawn.WorkTypeIsDisabled(workType);
+                                cachedEnabledWorkTypes.Add(workType);
+                            }
+                            else if (workType == WorkTypeDefOf.Crafting || workType == WorkTypeDefOf.Mining || workType == WorkTypeDefOf.Hauling || workType == WorkTypeDefOf.Doctor || workType == WorkTypeDefOf.Hunting || workType == WorkTypeDefOf.Construction || workType == WorkTypeDefOf.Growing || workType == AsimovDefOf.BasicWorker || workType == AsimovDefOf.Cooking || workType == WorkTypeDefOf.PlantCutting || workType == WorkTypeDefOf.Research || workType == AsimovDefOf.Cleaning || workType == WorkTypeDefOf.Firefighter || workType == AsimovDefOf.Tailoring || workType == AsimovDefOf.Art || workType == WorkTypeDefOf.Smithing || workType == AsimovDefOf.Warden || workType == WorkTypeDefOf.Handling)
+                            {
+                                cachedEnabledWorkTypes.Add(workType);
+                            }
+                        }
+                    }
+                }
+                return cachedEnabledWorkTypes;
+            }
+        }
+
+        public List<WorkTypeDef> cachedDisabledWorkTypes;
+
+        public List<WorkTypeDef> DisabledWorkTypes
+        {
+            get
+            {
+                if (cachedDisabledWorkTypes == null)
+                {
+                    cachedDisabledWorkTypes = new List<WorkTypeDef>();
+                    if (!Props.enableAllWorkTypes)
+                    {
+                        foreach (WorkTypeDef workType in DefDatabase<WorkTypeDef>.AllDefs)
+                        {
+                            if ((!EnabledWorkTypes.Contains(workType)) && !cachedDisabledWorkTypes.Contains(workType))
+                            {
+                                cachedDisabledWorkTypes.Add(workType);
+                            }
+                        }
+                    }
+                }
+                return cachedDisabledWorkTypes;
+            }
+        }
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+        }
+
+        public override void PostDestroy(DestroyMode mode, Map previousMap)
+        {
+            base.PostDestroy(mode, previousMap);
+        }
+
+        public void ResolveGraphics()
+        {
+            Pawn pawn = parent as Pawn;
+            if (pawn != null)
+            {
+                pawn.Drawer.renderer.SetAllGraphicsDirty();
+            }
+        }
+
+        public void ResolveWorkRestrictions()
+        {
+            cachedDisabledWorkTypes = null;
+            cachedEnabledWorkTypes = null;
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref skinFirst, "skinFirst");
+            Scribe_Values.Look(ref skinSecond, "skinSecond");
+            Scribe_Values.Look(ref resolved, "resolved");
+            Scribe_Collections.Look(ref extraEnabledWorkTypes, "extraEnabledWorkTypes");
+        }
+    }
+}

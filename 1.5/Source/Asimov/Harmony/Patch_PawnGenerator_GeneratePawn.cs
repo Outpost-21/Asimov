df79ce9312f1537dfc423ca27019c193d5921aae
neronix17 <39186138+neronix17@users.noreply.github.com>
2024-05-03 01:23:04 +0100

    1.0.8884

---
+++ 1.5/Source/Asimov/Harmony/Patch_PawnGenerator_GeneratePawn.cs
@@ -0,0 +1,26 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using HarmonyLib;
+using RimWorld.Planet;
+
+namespace Asimov
+{
+    [HarmonyPatch(typeof(PawnGenerator), "GeneratePawn", new Type[] {typeof(PawnGenerationRequest)})]
+    public static class Patch_PawnGenerator_GeneratePawn
+    {
+        [HarmonyPrefix]
+        public static void Prefix(PawnGenerationRequest request)
+        {
+            if(request.KindDef.race is PawnDef)
+            {
+                request.ForcedXenotype = XenotypeDefOf.Baseliner;
+            }
+        }
+    }
+}

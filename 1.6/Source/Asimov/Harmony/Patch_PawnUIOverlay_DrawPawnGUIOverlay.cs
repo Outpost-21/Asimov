d2b0e5bf62176c5118d68e315585f822c10f321e
Neronix17 <39186138+neronix17@users.noreply.github.com>
2025-10-16 13:23:41 +0100

    Fix for Automaton nameplate
    Player Animal-like nameplate was not displayed, as it didn't count as what RimWorld considers for displaying this information (player owned animal or colony mech), now it should.

---
+++ 1.6/Source/Asimov/Harmony/Patch_PawnUIOverlay_DrawPawnGUIOverlay.cs
@@ -0,0 +1,36 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Asimov
+{
+    [HarmonyPatch(typeof(PawnUIOverlay), "DrawPawnGUIOverlay")]
+    public static class Patch_PawnUIOverlay_DrawPawnGUIOverlay
+    {
+        [HarmonyPostfix]
+        public static void Postfix(PawnUIOverlay __instance)
+        {
+            Pawn pawn = __instance.pawn;
+            if(!pawn.Spawned || pawn.Map.fogGrid.IsFogged(pawn.Position) || WorldComponent_GravshipController.CutsceneInProgress)
+            {
+                return;
+            }
+            if (pawn.IsHumanlikeAutomaton())
+            {
+                return;
+            }
+            if (!pawn.IsPlayerAutomaton())
+            {
+                return;
+            }
+            Vector2 pos = GenMapUI.LabelDrawPosFor(pawn, -0.6f);
+            GenMapUI.DrawPawnLabel(pawn, pos);
+        }
+    }
+}

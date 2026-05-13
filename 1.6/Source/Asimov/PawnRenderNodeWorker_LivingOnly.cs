bc27efd2dd93e82113bb049da8c6f9f6e387e931
Neronix17 <39186138+neronix17@users.noreply.github.com>
2026-05-13 08:13:18 +0100

    Living Only Render Node
    Only draws render node if pawn is "living".

---
+++ 1.6/Source/Asimov/PawnRenderNodeWorker_LivingOnly.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Asimov
+{
+    public class PawnRenderNodeWorker_LivingOnly : PawnRenderNodeWorker
+    {
+        public override bool CanDrawNow(PawnRenderNode node, PawnDrawParms parms)
+        {
+            if (parms.dead) { return false; }
+            return base.CanDrawNow(node, parms);
+        }
+    }
+}

50739b003c5402e52e71c37850c39e37a1ff7ce3
Neronix17 <39186138+neronix17@users.noreply.github.com>
2026-05-18 19:17:20 +0100

    Repair toil was using Medicine instead of Crafting

--- 1.6/Source/Asimov/AI/JobDriver_RepairTarget.cs
+++ 1.6/Source/Asimov/AI/JobDriver_RepairTarget.cs
@@ -78,7 +78,7 @@ namespace Asimov
 				});
 			}
 			waitToil.FailOnCannotTouch(TargetIndex.A, interactionCell).WithProgressBarToilDelay(TargetIndex.A).PlaySustainerOrSound(SoundDefOf.Interact_Tend);
-			waitToil.activeSkill = () => SkillDefOf.Medicine;
+			waitToil.activeSkill = () => SkillDefOf.Crafting;
 			waitToil.handlingFacing = true;
 			waitToil.tickAction = delegate
 			{
